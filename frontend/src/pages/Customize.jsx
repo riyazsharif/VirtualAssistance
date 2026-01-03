10bd35ac2d35e21cb15808ba8606bd633ffdcea8
Riyaz sharif <165018259+riyazsharif@users.noreply.github.com>
2026-01-03 22:48:42 +0530

    Update Customize.jsx

--- frontend/src/pages/Customize.jsx
+++ frontend/src/pages/Customize.jsx
@@ -9,7 +9,7 @@ import image6 from "../assets/image6.jpeg"
 import image7 from "../assets/image7.jpeg"
 import { RiImageAddLine } from "react-icons/ri";
 import { MdKeyboardBackspace } from "react-icons/md";
-import { UserDataContext } from '../context/userContext'
+import { UserDataContext } from '../context/UserContext'
 import { useNavigate } from 'react-router-dom'
 function Customize() {
   const { serverUrl, userData,   setUserData,
