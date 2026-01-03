684f9634e78782a3b7f7930a9fd9fd11615546cb
Riyaz sharif <165018259+riyazsharif@users.noreply.github.com>
2026-01-03 22:52:33 +0530

    Update Card.jsx

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { UserDataContext } from '../context/userContext'
+import { UserDataContext } from '../context/UserContext'
 
 function Card({ image }) {
   const { serverUrl, userData,   setUserData,
