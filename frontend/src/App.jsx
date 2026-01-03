61c151f82e708124cf1584b4d88c6b6670aaa5fe
Riyaz sharif <165018259+riyazsharif@users.noreply.github.com>
2026-01-03 22:50:14 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import Customize from "./pages/Customize";
-import { UserDataContext } from "./context/userContext";
+import { UserDataContext } from "./context/UserContext";
 import Home from "./pages/Home";
 import Customize2 from "./pages/Customize2";
 
